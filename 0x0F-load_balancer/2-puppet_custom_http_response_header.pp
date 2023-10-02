6fa9a8018c8f58b2084249b2b285c8701c3f4f35
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-10-02 16:35:54 +0200

    feat: complete advanced task

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,21 @@
+# Use puppet to write custom headers for nginx
+# Its possible to use chained tasks that depend on each other
+
+exec {'apt_update':
+command => '/usr/bin/apt-get update',
+}
+package {'nginx':
+ensure => 'present'
+}
+
+file_line {'set custom http header':
+path  => '/etc/nginx/sites-enabled/default',
+match => 'server_name _;'
+line  => 'server_name _;\n\tadd_header X-Served-By \"${hostname}";'
+}
+
+exec {'restart nginx':
+command => 'usr/sbin/service nginx restart'
+}
+
+
