6d2a548df0718ebffa5035115165231c8552dbd8
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-10-02 11:33:47 +0200

    chore: move up redirection before restart

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -14,13 +14,15 @@ command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
 provider => shell,
 }
 
+
+exec {'sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.duckduckgo.com permanent;" /etc/nginx/sites-enabled/default':
+provider => shell,
+}
+
 exec {'run':
 command  => 'sudo service nginx restart',
 provider => shell,
 }
 
-exec {'sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.duckduckgo.com permanent;" /etc/nginx/sites-enabled/default':
-provider => shell,
-}
 
 
