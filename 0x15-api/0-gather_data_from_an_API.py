da013d62006089f1a248a6436fa9d0ddc4ed11cd
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-10-25 14:51:08 +0200

    fix: add userId param in url

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -14,11 +14,11 @@ if __name__ == "__main__":
     api_endpoint = "https://jsonplaceholder.typicode.com/{}"
     users_endpoint = "{}/{}".format(api_endpoint.format("users"), employee_id)
     todos_endpoint = api_endpoint.format("todos")
-
+    user_todos = "{}?userId={}".format(todos_endpoint, employee_id)
     employee_list = req.get(users_endpoint)
     employee_name = employee_list.json().get("name")
 
-    employee_todo = req.get(todos_endpoint)
+    employee_todo = req.get(user_todos)
     todo_list = employee_todo.json()
 
     completed = 0
