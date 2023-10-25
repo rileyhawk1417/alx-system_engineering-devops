bf466f910e1d0e45e1b1217d8ac5b4eb0b450440
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-10-25 15:57:34 +0200

    fix: remove arg guard

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -10,10 +10,6 @@ import requests as req
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: {} <employee_id>".format(sys.argv[0]))
-        sys.exit(1)
-
     employee_id = sys.argv[1]
     api_endpoint = "https://jsonplaceholder.typicode.com/{}"
     users_endpoint = "{}/{}".format(api_endpoint.format("users"), employee_id)
